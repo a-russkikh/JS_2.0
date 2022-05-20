36bc5a435669e9ac5a64daeaf581cb232b5e6a14
Alex <arusskikhqa@gmail.com>
2022-05-20 19:05:32 -0400

    feat:change HW for Lesson 7

--- HW/unit_07/unit_07/unit_07.js
+++ HW/unit_07/unit_07/unit_07.js
@@ -2,7 +2,9 @@
 // Напишите функцию t1, которая при нажатии кнопки выводит в out - 1 переменную a1.
 
 let a1 = 8;
-function t1() {}
+function t1() {
+  document.querySelector(".out-1").innerHTML = a1;
+}
 
 document.querySelector(".b-1").onclick = t1;
 
@@ -12,7 +14,7 @@ document.querySelector(".b-1").onclick = t1;
 
 let a2 = 8;
 function t2() {
-  //return
+  return a2;
 }
 
 document.querySelector(".b-2").onclick = function () {
@@ -22,7 +24,9 @@ document.querySelector(".b-2").onclick = function () {
 // Task 3.
 // Наша предыдущая функция, сильно все еще зависима от внешних переменных.Давайте сделаем ее более универсальной.Пусть функция t3 принимает 2 параметра и выводит их произведение.Допишите код функции так, чтобы она возвращала произведение двух чисел, переданных ей в качестве параметра a, b.Протестируем функцию на двух примерах, с помощью кнопок b-3-1 и b-3-2.
 
-function t3(a, b) {}
+function t3(a, b) {
+  return a * b;
+}
 
 document.querySelector(".b-3-1").onclick = function () {
   document.querySelector(".out-3-1").textContent = t3(3, 4);
@@ -34,7 +38,9 @@ document.querySelector(".b-3-2").onclick = function () {
 // Task 4
 // Напишите функцию t4 которая принимает ваш год рождения  и вычисляет ваш возраст.
 
-function t4(year) {}
+function t4(year) {
+  return 2022 - year;
+}
 
 document.querySelector(".b-4").onclick = function () {
   document.querySelector(".out-4").textContent = t4(1983);
@@ -43,7 +49,9 @@ document.querySelector(".b-4").onclick = function () {
 // Task 5
 // Напишите функцию t5, которая принимает ваше имя в качестве параметра и возвращает строку Hello name, где name - принятое в качестве параметра имя.
 
-function t5(yourName) {}
+function t5(yourName) {
+  return `Hello ${yourName}`;
+}
 
 document.querySelector(".b-5").onclick = function () {
   document.querySelector(".out-5").textContent = t5("Alex");
@@ -52,7 +60,10 @@ document.querySelector(".b-5").onclick = function () {
 // Task 6
 // Напишите функцию t6, которая принимает 2 числа и возвращает случайное целое число от первого до второго принятого параметра.
 
-function t6(a, b) {}
+function t6(a, b) {
+  console.log(b - a + a);
+  return Math.floor(Math.random() * (b - a) + a);
+}
 
 document.querySelector(".b-6").onclick = function () {
   document.querySelector(".out-6").textContent = t6(1, 7);
@@ -62,7 +73,7 @@ document.querySelector(".b-6").onclick = function () {
 // Напишите функцию t7, которая возвращает случайный цвет в формате rgb(x, y, z)(строка).Где x, y, z - случайные числа в диапазоне[0, 255].
 
 function t7() {
-  //return `rgb(${t6(0,255)}....допишите сами
+  return `rgb(${t6(0, 255)},${t6(0, 255)},${t6(0, 255)}`;
 }
 
 document.querySelector(".b-7").onclick = function () {
@@ -72,7 +83,9 @@ document.querySelector(".b-7").onclick = function () {
 // Task 8
 // Напишите функцию t8, которая принимает строку в качестве параметра и возвращает результат с очищенными пробелами в начале и вконце строки.Т.е.принимает _hello_(где знак _ символизирует пробел), а возвращает hello.Для удаления пробелов - используйте trim.
 
-function t8(str) {}
+function t8(str) {
+  return str.trim();
+}
 
 document.querySelector(".b-8").onclick = function () {
   document.querySelector(".out-8").textContent = t8("  Hello  ");
@@ -81,7 +94,13 @@ document.querySelector(".b-8").onclick = function () {
 // Task 9
 // Напишите функцию t9, которая принимает число и возвращает true, если число четное, и false если не четное.
 
-function t9(num) {}
+function t9(num) {
+  if (num % 2 == 1) {
+    return false;
+  } else {
+    return true;
+  }
+}
 
 document.querySelector(".b-9").onclick = function () {
   document.querySelector(".out-9").textContent = t9(15);
@@ -90,7 +109,14 @@ document.querySelector(".b-9").onclick = function () {
 // Task 10
 // Создайте функцию t10, которая принимает 2 числа и возвращает большее из них.В случае равенства - любое из чисел.
 
-function t10(a, b) {}
+function t10(a, b) {
+  if (a > b) {
+    return a;
+  } else if (a < b) {
+    return b;
+  }
+  return a;
+}
 
 document.querySelector(".b-10").onclick = function () {
   document.querySelector(".out-10").textContent = t10(3, 9);
